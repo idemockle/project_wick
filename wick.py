c3c139997d89e6fd72c3839b80c8973d7ebd5de4
idemockle <iank1226@yahoo.com>
2018-02-06 15:13:57 -0500

    Change spacing in TidyDF to follow PEP8

--- wick.py
+++ wick.py
@@ -11,18 +11,14 @@ class TidyDF:
         else:
             self._df = df.copy()
 
-
     def spread(self, key, value, fill=None, sep=None):
         return tidyr.spread(self, key, value, fill, sep)
-
 
     def gather(self):
         print('Not Implemented')
 
-
     def untidy(self):
         return self._df
-
 
     def __str__(self):
         n_head_rows = 5
@@ -31,10 +27,9 @@ class TidyDF:
             repstr += '\n + %i more rows'%(len(self._df)-n_head_rows)
         return repstr
 
-
     def __repr__(self):
         return self.__str__()
-
+    
 
 def tidy(df, inplace=False):
     return TidyDF(df, inplace=inplace)
