34838681a07ca3331010ca93b07d76dc8046c0c5
idemockle <iank1226@yahoo.com>
2018-02-11 20:14:24 -0500

    Add subset module with functions under "Subset" heading of data wrangling cheat sheet.

---
+++ wick/subset.py
@@ -0,0 +1,28 @@
+
+
+def select():
+    raise NotImplementedError
+
+
+def filter():
+    raise NotImplementedError
+
+
+def distinct():
+    raise NotImplementedError
+
+
+def sample_frac():
+    raise NotImplementedError
+
+
+def sample_n():
+    raise NotImplementedError
+
+
+def slice():
+    raise NotImplementedError
+
+
+def top_n():
+    raise NotImplementedError
